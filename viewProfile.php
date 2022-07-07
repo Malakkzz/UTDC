f5b0adb17a11bafe03b88d011aca3a9dff65aad8
Moh-jaber <jabermohamad1122@gmail.com>
2022-07-07 10:56:40 +0300

    database added

--- viewProfile.php
+++ viewProfile.php
@@ -6,23 +6,28 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>User profile</title>
     <style>
-        body{
-            background-color:#f4f4f4;
-        }
-        #btn-back{
-                position: fixed;
-                bottom: 50px;
-                right: 20px;
-            }
+    body {
+        background-color: #f4f4f4;
+    }
+
+    #btn-back {
+        position: fixed;
+        bottom: 50px;
+        right: 20px;
+        z-index: 1;
+    }
     </style>
 </head>
+
 <body>
     <h1 class="text-center my-5 py-4" style="color:#198754;">User Details</h1>
     <table class="table container w-50 my-5">
@@ -53,4 +58,5 @@
         </button>
     </form>
 </body>
+
 </html>
